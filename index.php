9952688b9665fa053f8c2eef3b0c4d416800ad89
yoelvinandra <yoelvinandra@gmail.com>
2025-08-04 12:39:05 +0700

    COMMING SOON

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Flavora | From Indonesian Soil to Global Soul</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <!-- Or use PNG -->
+    <!-- <link rel="icon" href="favicon.png" type="image/png"> -->
+  </head>
+  <body style="background:#F4E2D8; margin:0; height:100vh; display:flex; justify-content:center; align-items:center; flex-direction:column;">
+    <img src="logo.webp" alt="Logo" style="max-width: 100%; height: auto;">
+    <p style="margin-top:20px; font-family: Arial, sans-serif; font-size: 24px; color: #333;">COMING SOON</p>
+  </body>
+</html>
