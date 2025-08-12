128441003a07242bfbb8c4e6d2799866758da44a
NigelTheFrog <nkislew11@gmail.com>
2025-08-12 21:42:45 +0700

    section 2 ketinggalan - 12/08/2025

--- page/home-section/section2.php
+++ page/home-section/section2.php
@@ -1,7 +1,7 @@
 <section class="motto mt-5 mb-3 text-center">
   <div class="container">
     <h1 class="mb-3">Your trusted source of quality ingredients</h1>
-    <p style="max-width: 1050px;">At the heart of our mission is the commitment to provide our customers with top-notch, fresh bulk, and wholesale ingredients
+    <p>At the heart of our mission is the commitment to provide our customers with top-notch, fresh bulk, and wholesale ingredients
        - excellent for meeting daily needs and sparking innovation. 
        Consult our team of product experts for tailored guidance in meeting your needs.</p>
     
