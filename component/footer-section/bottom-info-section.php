ab4c0bd81fa10229d30e34ad0b8c52407cf4bfeb
NigelTheFrog <nkislew11@gmail.com>
2025-08-21 22:37:36 +0700

    layout fix - 21/08/2025

--- component/footer-section/bottom-info-section.php
+++ component/footer-section/bottom-info-section.php
@@ -1,60 +1,70 @@
 <section class="footer-info py-5">
-        <div class="container" style="max-width: 60vw;">
-            <div class="d-flex justify-content-between">
-                <div class="d-flex justify-content-evenly">
-                    <div>
-                        <h6 class="fw-bold">Flavora</h6>
-                        <ul class="list-unstyled">
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Products</a></li>
-                        </ul>
-                    </div>
-                    <div style="margin-left: 5vw;">
-                        <h6 class="fw-bold">Products</h6>
-                        <ul class="list-unstyled">
-                            <li><a href="#">Candlenut</a></li>
-                            <li><a href="#">Vanilla</a></li>
-                            <li><a href="#">Nutmeg</a></li>
-                            <li><a href="#">Javanese Corn</a></li>
-                            <li><a href="#">Cashew Nut</a></li>
-                            <li><a href="#">Peanut</a></li>
-                            <li><a href="#">Ginger</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div style="margin-left: 5vw;">
-                    <h6 class="fw-bold">Helpful Links</h6>
+    <div class="container" style="max-width: 60vw;">
+        <div class="d-flex justify-content-between">
+            <div class="d-flex justify-content-evenly">
+                <div>
+                    <h6 class="fw-bold">Flavora</h6>
                     <ul class="list-unstyled">
-                        <li><a href="#">Contact us</a></li>
-                        <li><a href="#">Sign up for news</a></li>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Products</a></li>
                     </ul>
                 </div>
-
+                <div style="margin-left: 5vw;">
+                    <h6 class="fw-bold">Products</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="#">Candlenut</a></li>
+                        <li><a href="#">Vanilla</a></li>
+                        <li><a href="#">Nutmeg</a></li>
+                        <li><a href="#">Javanese Corn</a></li>
+                        <li><a href="#">Cashew Nut</a></li>
+                        <li><a href="#">Peanut</a></li>
+                        <li><a href="#">Ginger</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div style="margin-left: 5vw;">
+                <h6 class="fw-bold">Helpful Links</h6>
+                <ul class="list-unstyled">
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Sign up for news</a></li>
+                </ul>
             </div>
 
-            <div class="d-flex justify-content-between" style="margin-top: 10vh;">
-                <div>
-                    <img src="assets/logo.webp" alt="Logo" class="mb-2" style="width: 100px;">
-                </div>
-                <div>
-                    <div class="d-flex justify-content-end">
-                        <a href="#" class="footer-button me-2"><i class="bi bi-linkedin fs-3"></i></a>
-                        <a href="https://youtube.com/@flavora_official?si=siHayF8aoWPS87NC" class="footer-button me-2"><i class="bi bi-youtube fs-3"></i></a>
-                        <a href="https://www.tiktok.com/@flavoraid_?_t=ZS-8yg6snws076&_r=1" class="footer-button me-2"><i class="bi bi-tiktok fs-3"></i></a>
-                        <a href="https://www.instagram.com/flavoraspices.id" class="footer-button "><i class="bi bi-instagram fs-3"></i></a>
-                    </div>
-                    <!-- <div style="text-align: right;">
+        </div>
+        <div class="row text-center text-md-start" style="margin-top: 10vh;">
+            <div class="col-12 col-md-auto mb-3 mb-md-0">
+                <img src="assets/logo.webp" alt="Logo" class="mb-2" style="width: 100px;">
+            </div>
+            <div class="col-12 col-md d-flex justify-content-center justify-content-md-end">
+                <a href="https://www.linkedin.com/in/flores-flavora-9a98a3377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app " class="footer-button me-2"><i class="bi bi-linkedin fs-3"></i></a>
+                <a href="https://youtube.com/@flavora_official?si=siHayF8aoWPS87NC" class="footer-button me-2"><i class="bi bi-youtube fs-3"></i></a>
+                <a href="https://www.tiktok.com/@flavoraid_?_t=ZS-8yg6snws076&_r=1" class="footer-button me-2"><i class="bi bi-tiktok fs-3"></i></a>
+                <a href="https://www.instagram.com/flavoraspices.id" class="footer-button "><i class="bi bi-instagram fs-3"></i></a>
+<!-- <div style="text-align: right;">
                         Copyright © 2025 Flavora Indonesia Limited. All Rights Reserved
                         Co Reg No: 1209910294A
                     </div> -->
-                    <!-- <div class="d-flex justify-content-end">
+                <!-- <div class="d-flex justify-content-end">
                         <a href="#" class="text-muted me-3">Privacy Policy</a>
                         <a href="#" class="text-muted me-3">Cookie Policy</a>
                         <a href="#" class="text-muted me-3">Sitemap</a>
                         <a href="#" class="text-muted">Disclaimer</a>
                     </div> -->
-                </div>
-
             </div>
         </div>
-    </section>
+        <!-- <div class="d-flex justify-content-between" style="margin-top: 10vh;">
+            <div>
+                <img src="assets/logo.webp" alt="Logo" class="mb-2" style="width: 100px;">
+            </div>
+            <div>
+                <div class="d-flex justify-content-end">
+                    <a href="https://www.linkedin.com/in/flores-flavora-9a98a3377?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app " class="footer-button me-2"><i class="bi bi-linkedin fs-3"></i></a>
+                    <a href="https://youtube.com/@flavora_official?si=siHayF8aoWPS87NC" class="footer-button me-2"><i class="bi bi-youtube fs-3"></i></a>
+                    <a href="https://www.tiktok.com/@flavoraid_?_t=ZS-8yg6snws076&_r=1" class="footer-button me-2"><i class="bi bi-tiktok fs-3"></i></a>
+                    <a href="https://www.instagram.com/flavoraspices.id" class="footer-button "><i class="bi bi-instagram fs-3"></i></a>
+                </div>               
+            </div>
+
+        </div> -->
+    </div>
+</section>
