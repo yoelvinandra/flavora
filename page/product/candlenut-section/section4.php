c0fae0f2d746da66a453652e0e36e18bc4d3071e
yoelvinandra <yoelvinandra@gmail.com>
2025-11-05 22:54:33 +0700

    UPDAGTE BAHASA PRODUK

--- page/product/candlenut-section/section4.php
+++ page/product/candlenut-section/section4.php
@@ -3,32 +3,32 @@
     <div class="applications-content">
       <div class="applications-title">
         <div class="title-separator"></div>
-        <h2>Applications of our candlenut</h2>
+        <h2><?=$_SESSION['lang']['product-application-candlenut']?></h2>
       </div>
       <div class="applications-grid">
         <div class="app-card">
-          <p>Seasoning</p>
+          <p><?=$_SESSION['lang']['product-application-seasoning']?></p>
           <!--merged image-->
           <div class="app-icon-wrapper">
             <img src="assets/product/candlenut/seasoning.png" class="app-icon" alt="Seasoning icon">
           </div>
         </div>
         <div class="app-card">
-          <p>Sauces and Dressing</p>
+          <p><?=$_SESSION['lang']['product-application-sauces-dressing']?></p>
           <!--merged image-->
           <div class="app-icon-wrapper">
             <img src="assets/product/candlenut/sauces-and-dressing.png" class="app-icon" alt="Sauces icon">
           </div>
         </div>
         <div class="app-card">
-          <p>Medicine</p>
+          <p><?=$_SESSION['lang']['product-application-medicine']?></p>
           <!--merged image-->
           <div class="app-icon-wrapper">
             <img src="assets/product/candlenut/medicine.png" class="app-icon" alt="Medicine icon">
           </div>
         </div>
         <div class="app-card">
-          <p>Snacks</p>
+          <p><?=$_SESSION['lang']['product-application-snacks']?></p>
           <!--merged image-->
           <div class="app-icon-wrapper">
             <img src="assets/product/candlenut/snack.png" class="app-icon" alt="Snacks icon">
