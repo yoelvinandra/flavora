eb7f63917c813f981b1485819383a553bff6dbfc
NigelTheFrog <nkislew11@gmail.com>
2025-11-20 00:37:51 +0700

    Update major product & article 20/11/2025

--- page/product/candlenut-section/section2.php
+++ page/product/candlenut-section/section2.php
@@ -1,3 +1,4 @@
 <section id="section-banner" class="banner">
-  <img src="assets/product/candlenut/banner.png" class="banner-image" alt="">
+  <!-- <img src="assets/product/candlenut/banner.png" class="banner-image" alt=""> -->
+     <!-- <img src="assets/product/candlenut/banner.webp" class="banner-image" alt=""> -->
 </section>
--- page/product/candlenut-section/section3.php
+++ page/product/candlenut-section/section3.php
@@ -36,7 +36,7 @@
           <p><?=$_SESSION['lang']['product-powder-detail']?></p>
           <div class="product-shop-button">
             <a href="#section-packaging-forms" class="btn primary"><?=$_SESSION['lang']['product-more-detail']?></a>
-            <a href="https://wa.me/6281138225758?text=Permisi, saya ingin bertanya mengenai Bubuk Kemiri Flavora. Bisa jelaskan kepada saya ?" class="btn secondary"><?=$_SESSION['lang']['product-shop-now']?></a>
+            <a href="https://wa.me/6281138225758?text=Permisi, saya ingin bertanya mengenai Bubuk Kemiri Flavora. Bisa jelaskan kepada saya ?" class="btn primary"><?=$_SESSION['lang']['product-shop-now']?></a>
           </div>
         </div>
         <div class="product-type-card">          
@@ -45,7 +45,7 @@
           <p><?=$_SESSION['lang']['product-shelled-detail']?></p>
           <div class="product-shop-button">
             <a href="#section-packaging-forms" class="btn primary"><?=$_SESSION['lang']['product-more-detail']?></a>
-            <a href="https://wa.me/6281138225758?text=Permisi, saya ingin bertanya mengenai Kemiri Kupas Flavora. Bisa jelaskan kepada saya ?" class="btn secondary"><?=$_SESSION['lang']['product-shop-now']?></a>
+            <a href="https://wa.me/6281138225758?text=Permisi, saya ingin bertanya mengenai Kemiri Kupas Flavora. Bisa jelaskan kepada saya ?" class="btn primary"><?=$_SESSION['lang']['product-shop-now']?></a>
           </div>
         </div>
         <div class="product-type-card">          
@@ -54,7 +54,7 @@
           <p><?=$_SESSION['lang']['product-oil-detail']?></p>
           <div class="product-shop-button">
             <a href="#section-packaging-forms" class="btn primary"><?=$_SESSION['lang']['product-more-detail']?></a>
-            <a href="https://wa.me/6281138225758?text=Permisi, saya ingin bertanya mengenai Minyak Kemiri Flavora. Bisa jelaskan kepada saya ?" class="btn secondary"><?=$_SESSION['lang']['product-shop-now']?></a>
+            <a href="https://wa.me/6281138225758?text=Permisi, saya ingin bertanya mengenai Minyak Kemiri Flavora. Bisa jelaskan kepada saya ?" class="btn primary"><?=$_SESSION['lang']['product-shop-now']?></a>
           </div>
         </div>
         
@@ -64,14 +64,20 @@
 <section id="section-packaging-forms" class="product-packaging-section-bg">
   <div class="product-packaging-section">
     <div class="packaging-header">
-      <div class="title-separator"></div>
+      <div class="title-separator" style="  background-color: var(--white);"></div>
       <h2><?=$_SESSION['lang']['product-packaging']?></h2>
     </div>
     <div class="packaging-wrapper">
       <button class="packaging-arrow prev" onclick="slidePackaging(this, true)">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
-          <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+          <path 
+            fill-rule="evenodd" 
+            stroke="currentColor" 
+            stroke-width="1" 
+            d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
+          />
         </svg>
+
       </button>
 
       <div class="packaging-carousel">
@@ -129,8 +135,13 @@
       </div>
 
       <button class="packaging-arrow next" onclick="slidePackaging(this, false)">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
-          <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+          <path 
+            fill-rule="evenodd" 
+            stroke="currentColor" 
+            stroke-width="1" 
+            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
+          />
         </svg>
       </button>
     </div>
@@ -139,12 +150,13 @@
 
 <section id="section-nutrition" class="nutrition-section">
   <div class="container">
-    <div class="nutrition-content">
-      <div class="nutrition-title">
-        <div class="title-separator"></div>
-        <h2><?=$_SESSION['lang']['product-nutrition']?></h2>
-        <h3>(per 100 gram)</h3>
-      </div>
+    <div class="nutrition-title">
+      <div class="title-separator"></div>
+      <h2><?=$_SESSION['lang']['product-nutrition']?></h2>
+      <h3>(per 100 gram)</h3>        
+    </div>
+
+    <div class="nutrition-content">    
       <div class="nutrition-table">
         <div class="nutrition-data-wrapper">
           <span class="nutrition-data-title">Crude Protein</span>
@@ -187,10 +199,24 @@
           <span class="nutrition-data-caption">1.252,58 ppm</span>
         </div>
         <div class="nutrition-data-wrapper">
-          <span class="nutrition-data-title" style="color:red">Total Aflatoxin</span>
-          <span class="nutrition-data-separator" style="border-bottom: 2px dotted red"></span>
-          <span class="nutrition-data-caption" style="color:red">0,0823 ppb</span>
+          <span class="nutrition-data-title" style="font-weight: bolder; font-style: italic; color: var(--semi-brown)">Total Aflatoxin</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-prescription2" viewBox="0 0 16 16">
+            <path d="M7 6h2v2h2v2H9v2H7v-2H5V8h2z"/>
+            <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4zM3 3h10V1H3z"/>
+          </svg>
+          <span class="nutrition-data-separator" style="border-bottom: 2px dotted var(--semi-brown);"></span>
+          <span class="nutrition-data-caption" style="font-weight: bolder; font-style: italic; color: var(--semi-brown)">0,0823 ppb</span>          
         </div>
+      </div>
+      <div class="nutrition-verified">
+        <img src="assets/certificate/sucofindo-cert-brown.webp" alt="Sucofindo Certification">
+        <span class="nutrition-verified-caption-normal">Analysis No.:</span>
+        <span class="nutrition-verified-caption-bold">E02503/FOBOAS</span>
+        <br>
+        <span class="nutrition-verified-caption-bold">BADAN PANGAN NASIONAL</span>
+        <span class="nutrition-verified-caption-bold">NO. 10/2024</span>
+        <span class="nutrition-verified-caption-normal">Aflatoksin B1 ≤ 10 ppb</span>
+        <span class="nutrition-verified-caption-normal">Aflatoksin Total ≤ 15 ppb</span>
       </div>
     </div>
   </div>
